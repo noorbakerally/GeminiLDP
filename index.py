f1a663ad6ecd546536a5e9bbce79e834429a7904
Noorani Bakerally <noorani.bakerally@gmail.com>
2017-07-16 15:47:44 +0200

    jinja delimeter changed

--- index.py
+++ index.py
@@ -3,6 +3,18 @@ from flask import render_template
 
 app = Flask(__name__)
 
+jinja_options = app.jinja_options.copy()
+
+jinja_options.update(dict(
+    block_start_string='<%',
+    block_end_string='%>',
+    variable_start_string='%%',
+    variable_end_string='%%',
+    comment_start_string='<#',
+    comment_end_string='#>'
+))
+app.jinja_options = jinja_options
+
 @app.route("/")
 def hello():
 	return render_template('index.html')
