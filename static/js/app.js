ae5fcb75dd60a277604d6167bac7405fe2222bed
Noorani Bakerally <noorani.bakerally@gmail.com>
2017-07-16 15:42:41 +0200

    variable delimeter changed

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,2 @@
 var app = angular.module('myApp', ['ngRoute','ui.bootstrap','treeControl']);
-app.config(['$interpolateProvider', function($interpolateProvider) {
-  $interpolateProvider.startSymbol('{a');
-  $interpolateProvider.endSymbol('a}');
-}]);
+
