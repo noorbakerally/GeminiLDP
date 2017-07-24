9f3551f39817eb93bcbfba9a32b1067efcfdc6a9
Noorani Bakerally <noorani.bakerally@gmail.com>
2017-07-24 13:34:36 +0200

    relative url added

--- static/js/services.js
+++ static/js/services.js
@@ -2,7 +2,7 @@ angular.module('myApp').factory('getDataService', ['$q','$http', function($q,$ht
     return {
         getData: function (node) {
             var deferred = $q.defer();
-            var url = "http://127.0.0.1:5000/getResource?ldpr="+node.iri;
+            var url = "/getResource?ldpr="+node.iri;
             $http.get(url).then(function(response) {deferred.resolve(response.data);});
             return deferred.promise;
         }
